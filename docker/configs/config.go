c5c4dc29dd6a283824c0bfa314966152edf8b5af
Adhitya Ardhiansyah <104175819+Adhitya2808@users.noreply.github.com>
2023-10-08 06:06:44 +0700

    Update config.go

--- docker/configs/config.go
+++ docker/configs/config.go
@@ -18,14 +18,23 @@ func init() {
 }
 
 func InitDB() {
-    connectionString := "root:password@tcp(34.123.248.159:3306)/test5?parseTime=true"
+    username := "root"
+    host := "34.123.248.159"
+    port := "3306"
+    name := "test5"
+
+    connectionString := fmt.Sprintf("%s@tcp(%s:%s)/%s?parseTime=true",
+        username,
+        host,
+        port,
+        name,
+    )
     var err error
     DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
     if err != nil {
         log.Fatalf("Failed to connect to database: %s", err.Error())
     }
 }
-
 func InitialMigration() {
     DB.AutoMigrate(&model.User{},&model.Book{})
 }
